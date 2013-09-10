2e7d1681cecbc381fc8e929fc5019d98f2f71868
JAEHYUN <JAEHYUN@172.30.3.217>
2013-09-10 22:37:22 +0900

    dd

--- CBMagazine/src/net/cb21/cbmagazine/SplashActivity.java
+++ CBMagazine/src/net/cb21/cbmagazine/SplashActivity.java
@@ -1,6 +1,47 @@
 package net.cb21.cbmagazine;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashActivity extends Activity {
-} // SplashActivity END
+
+	private static final int DELAY_TIME = 2000;
+
+	private int splashCount;
+	private ImageView ivKakao;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+//		setContentView(R.layout.splash_activity);
+
+//		ivKakao = (ImageView) findViewById(R.id.splash_activity_iv_kakao);
+		
+		splashCount = 0;
+
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+
+				if (splashCount == 0) {
+					splashCount++;
+					ivKakao.setAnimation(AnimationUtils.loadAnimation(getApplicationContext(), android.R.anim.fade_out));
+					ivKakao.setVisibility(View.INVISIBLE);
+					new Handler().postDelayed(this, DELAY_TIME);
+					return;
+				}
+				
+				startActivity(new Intent(SplashActivity.this, MainActivity.class));
+				
+				overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+				finish();
+
+			}
+		}, DELAY_TIME);
+	}
+}
